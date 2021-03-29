0c5076d322b41ec9cc550681c92d5232bb69770b
Gass-Dev <madebygass@gmail.com>
2021-03-29 18:05:32 +0200

    create task, fetch list tasks, edit task and delete task

--- back_end/main.py
+++ back_end/main.py
@@ -1,10 +1,71 @@
 # Imports
-from typing import Optional
+from typing import Optional, Task
+
 from fastapi import FastAPI
+# BDD:
+from pydantic import BaseModel
 
-app = FastAPI()
+
+# Use BaseModel
+class Task(BaseModel):
+
+    name: str
+    due_date: str
+    description: str
+
+app = FastAPI(title="Todo API")
+
+# Create, Read, Update, Delete
+store_tasks = []
+
+# Create Task
+@app.post('/task/')
+async def create_task(task: Task):
+    store_tasks.append(task)
+    return task
+
+# Fetch the list of tasks
+@app.get('/task/', response_model=Task[Task])
+async def get_all_tasks():
+    return store_tasks
+
+# Display Task Exception Not Found
+@app.get('/task/{id}')
+async def get_task(id: int):
+
+    try:
+
+        return store_tasks[id]
+
+    except:
+
+        raise HTTPException(status_code=404, detail="Task Not Found")
+
+# Update Task Except Not Found
+@app.put('/task/{id}')
+async def update_task(id: int, task: Task):
+
+    try:
+
+        store_tasks[id] = task
+        return store_tasks[id]
+
+    except:
+
+        raise HTTPException(status_code=404, detail="Task Not Found")
+
+# Delete Tasks Except Not Found
+@app.delete('/task/{id}')
+async def delete_task(id: int):
+
+    try:
+
+        obj =store_tasks[id]
+        store_tasks.pop(id)
+        return obj
+# pop() - Removes the element at the specified position
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+    except:
+
+        raise HTTPException(status=404, detail="Task Not Found")
